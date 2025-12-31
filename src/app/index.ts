04e5917f07a29a75be798b4ef057cbb241d4cfbc
mohitvashu1 <mohitchoubeybca@gmail.com>
2025-12-31 19:47:31 +0530

    {"data":{"sayHello":"Hello From Mohit To GraphQl Server"}}

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import {ApolloServer}  from '@apollo/server';
 import {expressMiddleware} from '@as-integrations/express4';
+import bodyParser from 'body-parser';
 
 
 
  export async function initServer() {
 
     const app =express();
+    app.use(bodyParser.json())
     const graphqlServer= new ApolloServer({
   typeDefs :`
   type Query{
